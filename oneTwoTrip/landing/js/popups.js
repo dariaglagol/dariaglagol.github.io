ceba9cfb0ba5b825514be3dea6b77bd0d53d8e0f
Daria Glagol <daria.glagol@roowix.com>
2019-04-29 11:54:16 +0700

    add some features

--- oneTwoTrip/landing/js/popups.js
+++ oneTwoTrip/landing/js/popups.js
@@ -1,209 +1,79 @@
-const railwaysBtn = document.querySelector('#railways');
-const aviaBtn = document.querySelector('#avia');
-const hotelsBtn = document.querySelector('#hotels');
-const popupNode = document.querySelector('#popup').content.querySelector('.popup-wrapper');
-const aviaNode = document.querySelector('#yield-avia').content;
-const railwaysNode = document.querySelector('#yield-railways').content;
-const hotelsNode = document.querySelector('#yield-hotels').content;
-const certificateNode = document.querySelector('#yield-certificates').content;
-const substrate = document.querySelector('.substrate');
-const certificateLinkDesktop = document.querySelector('.yield__certificate-link');
-const body = document.querySelector('body');
+(function(){
+    const yieldContainer = document.querySelector('.yield__price-list');
+    const popupNode = document.querySelector('#popup').content.querySelector('.popup-wrapper');
+    const aviaNode = document.querySelector('#yield-avia').content;
+    const railwaysNode = document.querySelector('#yield-railways').content;
+    const hotelsNode = document.querySelector('#yield-hotels').content;
+    const certificateNode = document.querySelector('#yield-certificates').content;
+    const substrate = document.querySelector('.substrate');
+    const certificateLinkDesktop = document.querySelector('.yield__certificate-link');
 
-const popup = popupNode.cloneNode(true);
+    const body = document.querySelector('body');
 
-const sliderList = document.querySelectorAll('.slider');
+    const popup = popupNode.cloneNode(true);
 
-const sliderMap = {};
-const sliderContainersList = {};
-const sliderContolsList = {};
+    function handleOpenVerticalPopups(e) {
+        const item = e.target;
+        const itemParent = item.parentElement;
+        const itemId = item.hasAttribute('id')
+            ? item.getAttribute('id') : itemParent.hasAttribute('id')
+            ? itemParent.getAttribute('id') : null;
+        const aviaText = aviaNode.cloneNode(true);
+        const hotelsText = hotelsNode.cloneNode(true);
+        const railwaysText = railwaysNode.cloneNode(true);
+        const certificateText = certificateNode.cloneNode(true);
 
-sliderList.forEach( node => {
-    const key = node.classList[1];
-    sliderMap[key] = node;
-});
-
-Object.keys(sliderMap).forEach((container, index) => {
-    const key = sliderMap[container].classList[1];
-    const sliderWrapper = sliderMap[container].children[0];
-    const control = sliderMap[container].children[1];
-    sliderContainersList[key] = sliderWrapper;
-    sliderContolsList[key] = control;
-
-});
-
-function handleOpenVerticalPopups() {
-    const item = event.target;
-    const itemId = item.hasAttribute('id') ? item.getAttribute('id') : null;
-    const aviaText = aviaNode.cloneNode(true);
-    const hotelsText = hotelsNode.cloneNode(true);
-    const railwaysText = railwaysNode.cloneNode(true);
-    const certificateText = certificateNode.cloneNode(true);
-
-    const verticalsMap = {
-        'railways': railwaysText,
-        'avia': aviaText,
-        'hotels': hotelsText,
-        'certificate': certificateText
-    };
-
-    if(itemId) {
-        body.style.overflow = 'hidden';
-        substrate.classList.remove('substrate--closed');
-
-        if(popup.children[1]) {
-            popup.removeChild(popup.children[1])
-        }
-
-        popup.appendChild(verticalsMap[itemId]);
-
-        sliderMap['yield__price-list-wrapper'].appendChild(popup);
-
-        const closeMenuBtn = popup.querySelector('.popup__btn--close');
-        closeMenuBtn.addEventListener('click', handleCloseVerticalPopups);
-    }
-
-    if(item === certificateLinkDesktop) {
-        body.style.overflow = 'hidden';
-        substrate.classList.remove('substrate--closed');
-
-        if(popup.children[1]) {
-            popup.removeChild(popup.children[1])
-        }
-
-        popup.appendChild(verticalsMap['certificate']);
-        sliderMap['yield__price-list-wrapper'].appendChild(popup);
-        const closeMenuBtn = popup.querySelector('.popup__btn--close');
-        closeMenuBtn.addEventListener('click', handleCloseVerticalPopups);
-    }
-}
-
-function handleCloseVerticalPopups() {
-    const popup = document.querySelector('.popup-wrapper');
-    const closeMenuBtn = popup.querySelector('.popup__btn--close');
-
-    substrate.classList.add('substrate--closed');
-    body.style.overflow = 'auto';
-    closeMenuBtn.removeEventListener('click', handleCloseVerticalPopups);
-    sliderContainersList['yield__price-list-wrapper'].addEventListener('click', handleOpenVerticalPopups);
-    popup.remove();
-}
-
-sliderContainersList['yield__price-list-wrapper'].addEventListener('click', handleOpenVerticalPopups);
-certificateLinkDesktop.addEventListener('click', handleOpenVerticalPopups);
-
-//SLIDER
-
-const verticalsPriceChildrenArray = [];
-const verticalsControlsChildrenArray = [];
-const advantagesPriceChildrenArray = [];
-const advantagesControlsChildrenArray = [];
-const ourClientsPriceChildrenArray = [];
-const ourClientsControlsChildrenArray = [];
-
-function getScreenWidth(){
-    const width = document.body.clientWidth;
-    if(width < 1280) {
-        createSlider();
-    }
-}
-
-for (let i = 0; i < sliderContainersList['yield__price-list-wrapper'].children.length; i++) {
-    verticalsPriceChildrenArray.push(sliderContainersList['yield__price-list-wrapper'].children[i]);
-    verticalsControlsChildrenArray.push(sliderContolsList['yield__price-list-wrapper'].children[i])
-}
-
-for (let i = 0; i < sliderContainersList['advantages__list-wrapper'].children.length; i++) {
-    advantagesPriceChildrenArray.push(sliderContainersList['advantages__list-wrapper'].children[i]);
-    advantagesControlsChildrenArray.push(sliderContolsList['advantages__list-wrapper'].children[i])
-}
-
-for (let i = 0; i < sliderContainersList['our-clients-list-wrapper'].children.length; i++) {
-    ourClientsPriceChildrenArray.push(sliderContainersList['our-clients-list-wrapper'].children[i]);
-    ourClientsControlsChildrenArray.push(sliderContolsList['our-clients-list-wrapper'].children[i])
-}
-
-window.addEventListener('resize', () => {
-    getScreenWidth()
-});
-
-window.addEventListener('load', () => {
-    getScreenWidth()
-});
-
-function createSlider() {
-    sliderContolsList['yield__price-list-wrapper'].addEventListener('click', (e) => {
-        const isControlInList = verticalsControlsChildrenArray.some((control) => control === e.target);
-        const elem = e.target;
-        if(isControlInList) {
-            cleanControls(elem, verticalsControlsChildrenArray);
-            const elemCount = verticalsControlsChildrenArray.indexOf(elem);
-            sliderContainersList['yield__price-list-wrapper'].style.transform = `translateX(-${elemCount * 100}%)`
-        }
-    });
-
-    sliderContolsList['advantages__list-wrapper'].addEventListener('click', (e) => {
-        const isControlInList = advantagesControlsChildrenArray.some((control) => control === e.target);
-        const elem = e.target;
-        if(isControlInList) {
-            cleanControls(elem, advantagesControlsChildrenArray);
-            const elemCount = advantagesControlsChildrenArray.indexOf(elem);
-            sliderContainersList['advantages__list-wrapper'].style.transform = `translateX(-${elemCount * 100}%)`
-        }
-    });
-
-    sliderContolsList['our-clients-list-wrapper'].addEventListener('click', (e) => {
-        const isControlInList = ourClientsControlsChildrenArray.some((control) => control === e.target);
-        const elem = e.target;
-        if(isControlInList) {
-            cleanControls(elem, ourClientsControlsChildrenArray);
-            const elemCount = ourClientsControlsChildrenArray.indexOf(elem);
-            sliderContainersList['our-clients-list-wrapper'].style.transform = `translateX(-${elemCount * 100}%)`
-        }
-    });
-}
-
-function cleanControls(elem, array) {
-    array.forEach(elem => elem.classList.remove('slider__control-item--active'));
-    elem.classList.add('slider__control-item--active');
-}
-
-//CAROUSEL
-
-const carouselToggleList = document.querySelectorAll('.tools__name-item');
-const carouselItemList = document.querySelectorAll('.tools__caption-item');
-
-const carouselToggleOptions = {
-    link: {},
-    deepLink: {},
-    banner: {},
-    searchForm: {},
-    searchFormWithRes: {},
-    apiAndWl: {}
-};
-
-const carouselItemOptions = {
-    link: {},
-    deepLink: {},
-    banner: {},
-    searchForm: {},
-    searchFormWithRes: {},
-    apiAndWl: {}
-};
-
-for (let i = 0; i < carouselToggleList.length; i++) {
-    Object.keys(carouselToggleOptions).forEach(key => {
-        carouselToggleOptions[key] = {
-            node: carouselToggleList[i],
-            clientWidth: carouselToggleList[i].getBoundingClientRect().width
+        const verticalsMap = {
+            'railways': railwaysText,
+            'avia': aviaText,
+            'hotels': hotelsText,
+            'certificate': certificateText
         };
-    }) ;
-}
 
-console.log(carouselItemOptions);
+        if(itemId) {
+            body.style.overflow = 'hidden';
+            substrate.classList.remove('substrate--closed');
 
+            if(popup.children[1]) {
+                popup.removeChild(popup.children[1])
+            }
 
+            popup.appendChild(verticalsMap[itemId]);
 
+            sliderMap['yield__price-list-wrapper'].appendChild(popup);
 
+            const closeMenuBtn = popup.querySelector('.popup__btn--close');
+            closeMenuBtn.addEventListener('click', handleCloseVerticalPopups);
+        }
 
+        if(item === certificateLinkDesktop) {
+            body.style.overflow = 'hidden';
+            substrate.classList.remove('substrate--closed');
+            substrate.classList.add('substrate--in-popup');
 
+            if(popup.children[1]) {
+                popup.removeChild(popup.children[1])
+            }
+
+            popup.appendChild(verticalsMap['certificate']);
+            sliderMap['yield__price-list-wrapper'].appendChild(popup);
+            const closeMenuBtn = popup.querySelector('.popup__btn--close');
+            closeMenuBtn.addEventListener('click', handleCloseVerticalPopups);
+        }
+    }
+
+    function handleCloseVerticalPopups() {
+        const popup = document.querySelector('.popup-wrapper');
+        const closeMenuBtn = popup.querySelector('.popup__btn--close');
+
+        substrate.classList.add('substrate--closed');
+        substrate.classList.remove('substrate--in-popup');
+        body.style.overflow = 'auto';
+        closeMenuBtn.removeEventListener('click', handleCloseVerticalPopups);
+        sliderContainersList['yield__price-list-wrapper'].addEventListener('click', handleOpenVerticalPopups);
+        popup.remove();
+    }
+
+    yieldContainer.addEventListener('click', handleOpenVerticalPopups);
+    certificateLinkDesktop.addEventListener('click', handleOpenVerticalPopups);
+})();
---
+++ oneTwoTrip/landing/js/sliders.js
@@ -0,0 +1,195 @@
+//SLIDER
+const sliderMap = {};
+const sliderContainersList = {};
+const sliderControlsList = {};
+
+const verticalsControlsChildrenArray = [];
+const advantagesControlsChildrenArray = [];
+const ourClientsControlsChildrenArray = [];
+const testimonialsControlsChildrenArray = [];
+
+const carouselWrappers = document.querySelectorAll('.carousel');
+const carouselToggleList = document.querySelector('.tools__name-list');
+const carouselItemList = document.querySelector('.tools__caption-list');
+
+const toolsControlsArray = [];
+const toolsItemsArray = [];
+const carouselMap = {};
+
+const sliderList = document.querySelectorAll('.slider');
+
+sliderList.forEach( node => {
+    const key = node.classList[1];
+    sliderMap[key] = node;
+});
+
+carouselWrappers.forEach(node => {
+    const key = node.classList[1];
+    carouselMap[key] = node;
+});
+
+Object.keys(sliderMap).forEach((container) => {
+    const key = sliderMap[container].classList[1];
+    const sliderWrapper = sliderMap[container].children[0];
+    const control = sliderMap[container].children[1];
+    sliderContainersList[key] = sliderWrapper;
+    sliderControlsList[key] = control;
+});
+
+function getScreenWidth(){
+    const width = document.body.clientWidth;
+    if(width > 1280) {
+        carouselToggleList.removeEventListener('click', createToolsCarousel);
+    }
+}
+
+function createSlidersFunctionalArrays(index, controlsArray, arraysKey) {
+    controlsArray.push(sliderControlsList[arraysKey].children[index])
+}
+
+for (let index = 0; index < sliderContainersList['yield__price-list-wrapper'].children.length; index++) {
+    createSlidersFunctionalArrays(index, verticalsControlsChildrenArray, 'yield__price-list-wrapper');
+}
+
+for (let index = 0; index < sliderContainersList['advantages__list-wrapper'].children.length; index++) {
+    createSlidersFunctionalArrays(index, advantagesControlsChildrenArray, 'advantages__list-wrapper');
+}
+
+for (let index = 0; index < sliderContainersList['our-clients-list-wrapper'].children.length; index++) {
+    createSlidersFunctionalArrays(index, ourClientsControlsChildrenArray, 'our-clients-list-wrapper');
+}
+
+for (let index = 0; index < sliderContainersList['testimonials__list-wrapper'].children.length; index++) {
+    createSlidersFunctionalArrays(index, testimonialsControlsChildrenArray, 'testimonials__list-wrapper');
+}
+
+window.addEventListener('load', () => {
+    getScreenWidth();
+    createSlider();
+});
+
+function addListener(e, controlsArray, activeClassName, arrayKey) {
+    const isControlInList = controlsArray.some((control) => control === e.target);
+    const elem = e.target;
+    if (isControlInList) {
+        cleanControls(elem, controlsArray, activeClassName);
+        const elemCount = controlsArray.indexOf(elem);
+        sliderContainersList[arrayKey].style.transform = `translateX(-${elemCount * 100}%)`
+    }
+}
+
+function createSlider() {
+    sliderControlsList['yield__price-list-wrapper'].addEventListener('click', (e) => {
+        addListener(e, verticalsControlsChildrenArray, 'slider__control-item--active', 'yield__price-list-wrapper')
+    });
+
+    sliderControlsList['advantages__list-wrapper'].addEventListener('click', (e) => {
+        addListener(e, advantagesControlsChildrenArray, 'slider__control-item--active', 'advantages__list-wrapper')
+    });
+
+    sliderControlsList['our-clients-list-wrapper'].addEventListener('click', (e) => {
+        addListener(e, ourClientsControlsChildrenArray, 'slider__control-item--active', 'our-clients-list-wrapper')
+    });
+
+    sliderControlsList['testimonials__list-wrapper'].addEventListener('click', (e) => {
+        addListener(e, testimonialsControlsChildrenArray, 'carousel__control-item--active', 'testimonials__list-wrapper')
+    });
+
+    carouselMap['tools__carousel'].addEventListener('click', (e) => {
+        createToolsCarousel(e, toolsControlsArray, 'carousel__toggle-item--active', carouselItemList);
+    });
+
+    carouselMap['testimonials__carousel'].addEventListener('click', (e) => {
+        createToolsCarousel(e, testimonialsControlsChildrenArray, 'carousel__control-item--active', sliderContainersList['testimonials__list-wrapper']);
+    })
+}
+
+function cleanControls(elem, array, className) {
+    console.log(elem)
+    array.forEach(elem => elem.classList.remove(className));
+    elem.classList.add(className);
+}
+
+//CAROUSEL
+
+for (let i = 0; i < carouselItemList.children.length; i++) {
+    toolsItemsArray.push(carouselItemList.children[i])
+}
+
+const carouselToggleOptions = {
+    link: {},
+    deepLink: {},
+    banner: {},
+    searchForm: {},
+    searchFormWithRes: {},
+    apiAndWl: {}
+};
+
+let containerTogglesShift = 0;
+let carouselItemCount = 0;
+
+Object.keys(carouselToggleOptions).forEach((key, index) => {
+    carouselToggleOptions[key] = {
+        node: carouselToggleList.children[index],
+        togglesShift: containerTogglesShift += carouselToggleList.children[index].getBoundingClientRect().width,
+        clientWidth: carouselToggleList.children[index].getBoundingClientRect().width
+    };
+    toolsControlsArray.push(carouselToggleList.children[index]);
+});
+
+function isTargetInControlList(target) {
+    const toggleArray = Object.keys(carouselToggleOptions);
+
+    const isInObject =  toggleArray.some(item => {
+        return carouselToggleOptions[item].node === target;
+    });
+
+    let togglesShift = 0;
+    let itemSize = 0;
+
+    for (let i = 0; i < toggleArray.length; i++) {
+        const item = toggleArray[i];
+        if (carouselToggleOptions[item].node === target) {
+            togglesShift = carouselToggleOptions[item].togglesShift;
+            itemSize = carouselToggleOptions[item].clientWidth;
+        }
+    }
+    return {isInObject, togglesShift, itemSize}
+}
+
+function createToolsCarousel(e, arrayItems, activeClassName, nodeElement) {
+    const target = e.target;
+    if (isTargetInControlList(target).isInObject) {
+        const togglesShift = isTargetInControlList(target).togglesShift;
+        const itemSize = isTargetInControlList(target).itemSize;
+        const targetCount = toolsControlsArray.indexOf(target);
+        cleanControls(target, toolsControlsArray, 'carousel__toggle-item--active');
+        carouselToggleList.style.transform = `translateX(calc(50% - (-${itemSize / 2}px + ${togglesShift}px)))`;
+        carouselItemList.style.transform = `translateX(-${targetCount * 100}%)`;
+        target.classList.add('carousel__toggle-item--active');
+    }
+
+    const detectArrow = target.classList.contains('carousel__arrow--left')
+        ? 'left' : target.classList.contains('carousel__arrow--right')
+        ? 'right': null;
+
+    if (detectArrow) {
+        addArrowControls(detectArrow, arrayItems, activeClassName, nodeElement);
+    }
+}
+
+function addArrowControls(arrow, arrayItems, activeClassName, nodeElement) {
+    if (arrow === 'right' && carouselItemCount < arrayItems.length - 1) {
+        carouselItemCount++;
+        cleanControls(arrayItems[carouselItemCount], arrayItems, activeClassName);
+        nodeElement.style.transform = `translateX(-${carouselItemCount * 100}%)`;
+        return carouselItemCount
+    } else if (arrow === 'left' && carouselItemCount > 0) {
+        carouselItemCount--;
+        cleanControls(arrayItems[carouselItemCount], arrayItems, activeClassName);
+        nodeElement.style.transform = `translateX(-${carouselItemCount * 100}%)`;
+        return carouselItemCount
+    }
+}
+
+// carouselWrapper.addEventListener('click', createToolsCarousel);
