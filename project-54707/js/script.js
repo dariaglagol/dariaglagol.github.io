4a9018aa9bf25ecdc3b83ee88a30c8f93b315e5a
Daria Glagol <daria.glagol@gmail.com>
2019-06-14 21:13:13 +0700

    54707

---
+++ project-54707/js/script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector(".modal");
+let modal = document.querySelector(".modal-form");
+
+btn.addEventListener("click", function () {
+  modal.classList.toggle("show");
+});
+
+
