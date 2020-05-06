6031e1f9f4ed94fa11d724b7a329ef0b3d32f373
Daria Glagol <daria.glagol@gmail.com>
2020-05-06 09:24:44 +0700

    add Veeam test task

--- scrollTest/js/search-hotel-form.js
+++
@@ -1,7 +0,0 @@
-    var link = document.querySelector(".btn-search-hotel");
-    var popup = document.querySelector(".search-hotel-form");
-        
-    link.addEventListener("click", function (evt) {
-        evt.preventDefault();
-        popup.classList.toggle("search-hotel-form-none");
-    });
